1a6d859cfe2826c7da00655c85017f3aca20b83f
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-23 16:17:39 +0100

    aggiungo metodo map per recuperare nomi e classi

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("js ok");
+/*
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.*/
+
+//let iconName = [];
+
+//const icon = icons.forEach((element) => {
+//  iconName.push(element.name);});
+
+//Uso il metodo map per recuperare tutti i nomi e le classi
+const iconName = icons.map((icon) => {
+  return icon.name;
+});
+
+console.log(iconName);
+
+const iconClass = icons.map((icon) => {
+  return icon.prefix;
+});
+
+console.log(iconClass);
