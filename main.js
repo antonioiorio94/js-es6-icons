e23c8f48252ec9cf07bdebcd3acfa03296bab4a8
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-23 18:02:25 +0100

    aggiunta event listener sul value

--- main.js
+++ main.js
@@ -2,27 +2,20 @@
 Milestone 1
 Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.*/
 
+/*
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.*/
+
+//PROVE
 //let iconName = [];
 
 //const icon = icons.forEach((element) => {
 //  iconName.push(element.name);});
 
-//Uso il metodo map per recuperare tutti i nomi e le classi
-const iconName = icons.map((icon) => {
-  return icon.name;
-});
-
-console.log(iconName);
-
-const iconClass = icons.map((icon) => {
-  return icon.prefix;
-});
-
-console.log(iconClass);
-
 const divContainer = document.getElementById("container");
 
-//Uso forEach per inserire name e prefix nei div
+//Uso forEach per inserire name,prefix e color nei div
+
 icons.forEach(({ name, prefix, color }) => {
   const divIconBox = document.createElement("div");
   divIconBox.setAttribute("class", " icon_box");
@@ -36,6 +29,21 @@ icons.forEach(({ name, prefix, color }) => {
   divContainer.appendChild(divIconBox);
 });
 
-/*
-Milestone 2
-Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.*/
+/*Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.*/
+
+//Uso il metodo map per recuperare tutti i tipi
+const iconType = icons.map((icon) => {
+  return icon.type;
+});
+
+console.log(iconType);
+
+//Event listener sul value del selector
+let valueOfSelect = document
+  .getElementById("selector")
+  .addEventListener("change", function () {
+    console.log(this.value);
+  });
+
+console.log(valueOfSelect);
